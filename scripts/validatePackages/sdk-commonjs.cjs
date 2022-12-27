37c581f04d1c9f16535f705591192086c961649f
John Callahan <jocallah@akamai.com>
2022-12-27 17:02:36 -0500

    Update

--- scripts/validatePackages/sdk-commonjs.cjs
+++ scripts/validatePackages/sdk-commonjs.cjs
@@ -1,3 +1,5 @@
+// This file used by GitHub Actions (ci.yml workflow) to validate the api-v4 NPM package before publishing.
+
 const apiv4 = require('@linode/api-v4/lib');
 
 apiv4.getRegions().then(res => {
--- scripts/validatePackages/sdk-esm.js
+++ scripts/validatePackages/sdk-esm.js
@@ -1,3 +1,5 @@
+// This file used by GitHub Actions (ci.yml workflow) to validate the api-v4 NPM package before publishing.
+
 import { getRegions } from '@linode/api-v4';
 
 getRegions().then(res => {
